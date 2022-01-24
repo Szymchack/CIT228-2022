d2693073bbe4cb7fa5bac5f8dceead4bcbc44dd8
Szymchack <44557214+Szymchack@users.noreply.github.com>
2022-01-23 19:12:07 -0500

    Add files via upload

---
+++ Chapter 2/numberFun.py
@@ -0,0 +1,25 @@
+import random
+number1 = random.randrange(100)
+number2 = random.randrange(100)
+print(number1)
+print(number2)
+print("Number 1 = 88")
+print("Number 2 = 3")
+x=88
+y=3
+print(x//y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x+y)
+print(x-y)
+print(x*y)
+
+NUMBER2 = 21
+print(x//NUMBER2)
+print(x/NUMBER2)
+print(x%NUMBER2)
+print(x**NUMBER2)
+print(x+NUMBER2)
+print(x-NUMBER2)
+print(x*NUMBER2)
---
+++ Chapter 2/stringFun.py
@@ -0,0 +1,55 @@
+print("-------------------------------------------")
+print("Exercise 1")
+name="cathyann szymchack"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My First initial is:", name[0].upper())
+
+print("-------------------------------------------")
+print("Exercise 2")
+noun = "frog"
+adj = "happy"
+verb = "croaked"
+ending = "all through the night."
+sentence1 = "the " + adj + " orange " +  noun +" " + verb + " " + ending
+sentence2 = f"the {adj} orange {noun} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+
+print("--------------------------------------------")
+print("Exercise 3")
+beginning = "Someday"
+noun = " you"
+verb = " will cry "
+ending = "for you!"
+adj = "just like"
+noun2 =" I"
+sentence1 = beginning + noun + verb
+sentence2 = "for me,"
+sentence3 = adj + noun2 + " cried "
+sentence4 = ending
+
+print (sentence1)
+print ("\t", sentence2)
+print (sentence3)
+print ("\t", sentence4)
+
+
+print("----------------------------------------------")
+print("Exercise 4")
+string1 = "Movie"
+string2 = "TV show"
+value1 = "Red"
+value2 = "Survivor"
+value3 = "The Curse of Oak Island-Must Watch"
+value4 = "Olympus Has Fallen"
+value5 = "Race To Witch Mountain"
+value6 = "Expedition Unknown"
+print(string1,"\t\t",string2,"\n")
+print(value1,"\n")
+print("\t\t",value2)
+print("\t\t",value3)
+print(value4,"\n")
+print(value5,"\n")
+print("\t\t", value6)
